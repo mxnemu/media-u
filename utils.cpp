66f61e3fc789e1fbac37a121f55e7526e45e9e38
Nehmulos <nehmu@lavabit.com>
2013-06-01 22:11:07 +0200

    fix a wrong index acess bug (copypasta); use a simple, but actually working wordsplit on space function;

--- utils.cpp
+++ utils.cpp
@@ -8,15 +8,15 @@ Utils::Utils()
 int Utils::querySimiliarity(const QString a, const QString b) {
     QRegExp wordSplitRegex("(\\b)?.+?(\\s|\\b|$)");
     wordSplitRegex.indexIn(a);
-    QStringList aWords = wordSplitRegex.capturedTexts();
+    QStringList aWords = a.split(" ");
     wordSplitRegex.indexIn(b);
-    QStringList bWords = wordSplitRegex.capturedTexts();
+    QStringList bWords = b.split(" ");
 
     int matches = 0;
     for (int i=0; i < aWords.length(); ++i) {
         const QString& aWord = aWords.at(i).trimmed();
         for (int j=0; j < bWords.length(); ++j) {
-            const QString bWord = bWords.at(i).trimmed();
+            const QString bWord = bWords.at(j).trimmed();
             if (bWord == aWord) {
                 matches += 1;
                 break;
