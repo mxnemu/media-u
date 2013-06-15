e0fe13cd2e0a005fa7bc115ffa9d8f3cdf5e3715
Nehmulos <nehmu@lavabit.com>
2013-06-15 21:49:47 +0200

    added a nogui startarg that launches the app without showing a window; don't start with invalid arguments;

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ int main(int argc, char *argv[]) {
 
     QString configPath;
     bool fullscreen = false;
+    bool noGui = false;
 
     for (int i=0; i < argc; ++i) {
 
@@ -36,16 +37,24 @@ int main(int argc, char *argv[]) {
             }
         } else if (strcmp(argv[i], "--fullscreen") == 0) {
             fullscreen = true;
+        } else if (strcmp(argv[i], "--nogui") == 0) {
+            noGui = true;
         } else if (strcmp(argv[i], "--help") == 0) {
             std::cout << "A Qt Application to manage and play your media library.\n\n";
             std::cout << "available Arguments:\n";
             std::cout << "--fullscreen\n";
             std::cout << "    launch qt gui in fullscreen.\n";
+            std::cout << "--nogui\n";
+            std::cout << "    launch without showing a window. The Api and mplayer should still work.\n";
             std::cout << "--configdir %dir\n";
             std::cout << "    %dir path to a directory where configs shall be written/read\n";
             std::cout << "    if %dir does not exist it will be created. If it can't be created\n";
             std::cout << "    .media-u in the home directory will be the default configdir.\n";
             std::cout.flush();
+            return 0;
+        } else if (i > 0) {
+            std::cout << "unknown parameter " << i << ": " << argv[i] << '\n';
+            std::cout << "use --help for a parameter list." << std::endl;
             return 0;
         }
     }
@@ -56,10 +65,12 @@ int main(int argc, char *argv[]) {
     library.readAll();
 
     MainWindow w(library);
-    if (fullscreen) {
-        w.showFullScreen();
-    } else {
-        w.show();
+    if (!noGui) {
+        if (fullscreen) {
+            w.showFullScreen();
+        } else {
+            w.show();
+        }
     }
 
     VideoPlayer* player = new Mplayer(); // TODO get player from config
