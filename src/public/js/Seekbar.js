1be42dd118116805eaff6a86e86c509a5f949f97
Nehmulos <nehmu@lavabit.com>
2013-09-21 15:49:42 +0200

    detect if the currently playing file has changed and reset the seekbar and thumbnailcache;

--- src/public/js/Seekbar.js
+++ src/public/js/Seekbar.js
@@ -4,14 +4,27 @@ function Progress(page, onUpdateCallback) {
     this.metaData = null;
     this.page = page;
     this.onUpdateCallback = onUpdateCallback;
+    this.onResetCallback = null;
 }
 
 Progress.prototype.fetchMetaData = function(callback) {
     var self = this;
+    
     $.getJSON("api/player/metaData", function(data) {
+        if (data.duration < 0) {
+            window.setTimeout(function() {
+                self.fetchMetaData(callback);
+            }, 500);
+            return;
+        }
         self.metaData = data;
         self.startUp();
-        callback();
+        if (self.onResetCallback) {
+            self.onResetCallback();
+        }
+        if (callback) {
+            callback();
+        }
     });
 }
 
@@ -37,8 +50,7 @@ Progress.prototype.update = function(second) {
     this.seconds = Math.min(second, this.metaData.duration);
     this.onUpdateCallback(second, this.metaData.duration)
     if (this.seconds >= this.metaData.duration) {
-        window.clearInterval(this.progressUpdateIntervalId);
-        window.clearInterval(this.progressSyncIntervalId);
+        this.resync();
     }
 }
 
@@ -46,6 +58,12 @@ Progress.prototype.resync = function() {
     var self = this;
     $.getJSON("api/player/progress", function(d) {
         self.update(d.seconds);
+        console.log(d.path);
+        if (d.path != self.path) {
+            window.clearInterval(this.progressUpdateIntervalId);
+            window.clearInterval(this.progressSyncIntervalId);
+            self.fetchMetaData();
+        }
     });
 }
 
@@ -77,79 +95,86 @@ Seekbar.prototype.setTooltip = function(time, x, y, img, chapter) {
     this.tooltip.css("top", y);
 }
 
+Seekbar.prototype.reset = function() {
+    this.thumbnailCache = new ThumbnailCache();
+    if (G.preloadSeekBarPreviews) {
+        this.thumbnailCache.loadAll(this.progress.metaData.duration);
+    }
+}
+
+Seekbar.prototype.bindListeners = function() {
+    var self = this;
+
+    this.bar.mousemove(function(event) {
+        var x = Math.max(event.clientX, 50);
+        x = Math.min(x, window.innerWidth - 50);
+        var y = self.bar.offset().top;
+        
+        var videoPos = self.progress.metaData.duration * 
+                       (event.clientX / window.innerWidth);
+        var minute = (videoPos / 60);
+        var second = videoPos % 60;
+        
+        var chapter = null;
+        for (var i=0; i < self.progress.metaData.chapters.length; ++i) {
+            if (videoPos > self.progress.metaData.chapters[i].start &&
+                videoPos < self.progress.metaData.chapters[i].end) {
+                
+                chapter = self.progress.metaData.chapters[i].title;
+            }
+        }
+        function setTooltip(img) {
+            self.setTooltip(
+                {
+                    total: videoPos,
+                    minute: minute,
+                    second: second
+                },
+                x,
+                y,
+                img,
+                chapter
+            );
+        }
+        setTooltip();
+        self.thumbnailCache.get(videoPos, setTooltip);
+    });
+    this.bar.mouseenter(function(event) {
+        self.tooltip.show();
+    });
+    this.bar.mouseleave(function(event) {
+        self.tooltip.hide();
+    });
+        
+    this.bar.click(function(event) {
+        var videoPos = self.progress.metaData.duration * 
+                       (event.clientX / window.innerWidth);
+        $.getJSON("api/player/jumpTo?" + Math.floor(videoPos), function() {
+            self.page.setPauseDisplay("unPaused");
+            self.progress.update(videoPos);
+        });
+    });
+}
+
 Seekbar.prototype.createNodes = function() {
-    var bar = $(document.createElement("div"));
-    bar.addClass("seekbar");
+    this.bar = $(document.createElement("div"));
+    this.bar.addClass("seekbar");
     
     this.tooltip = $(document.createElement("div"));
     this.tooltip.addClass("seekbartooltip");
     
     this.progressBar = document.createElement("div");
     this.progressBar.className = "progressBar";
-    bar.append(this.progressBar);
+    this.bar.append(this.progressBar);
     
     var self = this;
     this.progress = new Progress(this.page, function(second, maximum) {
         self.progressBar.style.width = ((second/maximum)*100) + "%";
     });
-    
+    this.progress.onResetCallback = function() {
+        self.reset();
+    }
     this.progress.fetchMetaData(function() {
-
-        var thumbnailCache = new ThumbnailCache();
-        if (G.preloadSeekBarPreviews) {
-            thumbnailCache.loadAll(self.progress.metaData.duration);
-        }    
-    
-        bar.mousemove(function(event) {
-            var x = Math.max(event.clientX, 50);
-            x = Math.min(x, window.innerWidth - 50);
-            var y = bar.offset().top;
-            
-            var videoPos = self.progress.metaData.duration * 
-                           (event.clientX / window.innerWidth);
-            var minute = (videoPos / 60);
-            var second = videoPos % 60;
-            
-            var chapter = null;
-            for (var i=0; i < self.progress.metaData.chapters.length; ++i) {
-                if (videoPos > self.progress.metaData.chapters[i].start &&
-                    videoPos < self.progress.metaData.chapters[i].end) {
-                    
-                    chapter = self.progress.metaData.chapters[i].title;
-                }
-            }
-            function setTooltip(img) {
-                self.setTooltip(
-                    {
-                        total: videoPos,
-                        minute: minute,
-                        second: second
-                    },
-                    x,
-                    y,
-                    img,
-                    chapter
-                );
-            }
-            setTooltip();
-            thumbnailCache.get(videoPos, setTooltip);
-        });
-        bar.mouseenter(function(event) {
-            self.tooltip.show();
-        });
-        bar.mouseleave(function(event) {
-            self.tooltip.hide();
-        });
-            
-        bar.click(function(event) {
-            var videoPos = self.progress.metaData.duration * 
-                           (event.clientX / window.innerWidth);
-            $.getJSON("api/player/jumpTo?" + Math.floor(videoPos), function() {
-                self.page.setPauseDisplay("unPaused");
-                self.progress.update(videoPos);
-            });
-        });
+        self.bindListeners();
     });
-        
-    this.bar = bar;
 }
