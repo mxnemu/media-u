c277ede6b4002a1a88173469f20951a2c9d774be
Nehmulos <nehmu@lavabit.com>
2013-11-16 00:28:28 +0100

    set custom useragent for the official mal api; todo write message to mal to whitelist my useragent;

--- src/malclient.cpp
+++ src/malclient.cpp
@@ -115,6 +115,7 @@ CURL* MalClient::curlClient(const char* url, CurlResult& userdata) {
     curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, CurlResult::write_data);
     curl_easy_setopt(handle, CURLOPT_TIMEOUT, 15);
     curl_easy_setopt(handle, CURLOPT_NOSIGNAL, 1);
+    curl_easy_setopt(handle, CURLOPT_USERAGENT, "nemu-malapiclient");
     curl_easy_setopt(handle, CURLOPT_WRITEDATA, &userdata);
     return handle;
 }
