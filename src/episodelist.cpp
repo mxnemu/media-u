0476b293cf113b03a398b7f29408fe41a37ea7ba
Nehmulos <nehmu@lavabit.com>
2014-03-27 01:04:01 +0100

    fix sub-epnum skip in setMaximum/MinimumWatched

--- src/episodelist.cpp
+++ src/episodelist.cpp
@@ -179,7 +179,10 @@ void EpisodeList::setMinimalWatched(int number) {
     foreach (Episode* ep, episodes) {
         float epNum = ep->getEpisodeNumber();
         // ignore 11.5 style special episodes
-        if (epNum <= number && epNum > 0 && 0 == epNum - (int)epNum) {
+        if (epNum <= 0 || 0 != epNum - (int)epNum) {
+            continue;
+        }
+        if (epNum <= number) {
             ep->setWatched(true);
         }
     }
@@ -189,7 +192,7 @@ void EpisodeList::setMaximalWatched(int number) {
     foreach (Episode* ep, episodes) {
         float epNum = ep->getEpisodeNumber();
         // ignore 11.5 style special episodes
-        if (epNum > 0 && 0 == epNum - (int)epNum) {
+        if (epNum <= 0 || 0 != epNum - (int)epNum) {
             continue;
         }
         if (epNum <= number) {
